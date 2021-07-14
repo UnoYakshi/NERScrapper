e6901ca966c51800e60f1198da7dbf638de87e50
Uno Yakshi <uno.yakshi@gmail.com>
2021-07-14 18:19:21 +0300

    Fixed the main test...

--- nerscrap/tests/test_nerscrap.py
+++ nerscrap/tests/test_nerscrap.py
@@ -1,19 +1,30 @@
+from typing import List
+
+import pytest
 from fastapi.testclient import TestClient
 from nerscrap.main import app
 
 
 client = TestClient(app)
 
-URLS = [
-    # 'https://google.com',
-    # 'http://yakshi.uno',
-    # 'https://hands.ru/company/about',
-    # 'https://repetitors.info',
-    'https://rosreestr.gov.ru/site/'
-]
+URLS_DATA = {
+    'https://www.regextester.com/94215': ['79855310868',
+                                          '79855310868',
+                                          '880084545454',
+                                          '880084545454',
+                                          '784545487878',
+                                          '7889213554654',
+                                          '84456464641',
+                                          '89855310868'],
+    'http://yakshi.uno': [],
+    'https://hands.ru/company/about': [],
+    'https://repetitors.info': ['880050572838'],
+    'https://rosreestr.gov.ru/site/': []
+}
 
 
-def test_phones():
-    response = client.get("/phones", json={'urls': URLS})
+@pytest.mark.parametrize('url,expected', [(item[0], item[1]) for item in URLS_DATA.items()])
+def test_phones(url: str, expected: List[str]):
+    response = client.get("/phones", json={'urls': [url]})
     assert response.status_code == 200
-    # assert response.json() == {'phones': []}
+    assert response.json() == expected
